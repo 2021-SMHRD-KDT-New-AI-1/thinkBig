8c8c92adb2bbbfc71dbc6a53f54cc31e5eae8198
thinkBig <jefron1100@gmail.com>
2021-10-07 12:11:12 +0900

    주석이 잘 들어갔는지 확인하는 commit

--- app/src/main/java/com/cjt/weather/MainActivity.java
+++ app/src/main/java/com/cjt/weather/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 님들 주석 잘 달아주시고
+        // 이 프로젝트 파일로 시작해봅시다!!
     }
 }
