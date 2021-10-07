51d0343d990752541c0b24cbe96613d209cde71d
yunivearth <980v0b@gmail.com>
2021-10-07 12:31:45 +0900

    지윤이 commit

---
+++ app/src/main/java/com/cjt/weather/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.cjt.weather;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText et_id, et_pw;
+    Button btn_login, btn_findid, btn_findpw, btn_join;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_id = findViewById(R.id.et_id);
+        et_pw = findViewById(R.id.et_pw);
+        btn_login = findViewById(R.id.btn_login);
+        btn_findid = findViewById(R.id.btn_findid);
+        btn_findpw = findViewById(R.id.btn_findpw);
+        btn_join = findViewById(R.id.btn_join);
+
+    }
+}
