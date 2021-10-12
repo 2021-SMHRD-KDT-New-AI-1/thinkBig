da7d63f997c806b5eda69fdeb3b42a5feac18526
yunivearth <980v0b@gmail.com>
2021-10-12 17:54:32 +0900

    지윤 commit: submainactivity

---
+++ app/src/main/java/com/cjt/weather/SubmainActivity.java
@@ -0,0 +1,14 @@
+package com.cjt.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubmainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submain);
+    }
+}
