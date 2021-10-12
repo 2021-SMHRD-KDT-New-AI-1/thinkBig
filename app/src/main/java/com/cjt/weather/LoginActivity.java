541b4b9d23b983603e410d370034e649a228d894
leedohyeon7 <em1000s@naver.com>
2021-10-12 12:04:19 +0900

    도현: 화면이동구현

--- app/src/main/java/com/cjt/weather/LoginActivity.java
+++ app/src/main/java/com/cjt/weather/LoginActivity.java
@@ -3,7 +3,10 @@ package com.cjt.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         et_id = findViewById(R.id.et_id);
         et_pw = findViewById(R.id.et_pw);
@@ -23,6 +26,22 @@ public class LoginActivity extends AppCompatActivity {
         btn_findid = findViewById(R.id.btn_findid);
         btn_findpw = findViewById(R.id.btn_findpw);
         btn_join = findViewById(R.id.btn_join);
+
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent(출발 Activity.this, 도착 Activity.class)
+                Intent intent = new Intent(LoginActivity.this,sign_up.class);
+                startActivity(intent);
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
